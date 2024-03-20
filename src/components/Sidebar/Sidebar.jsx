cfe7e951bea49b31d8ea8b59f7d0cd8082db028c
Jaeyong <wodyd8672@gmail.com>
2024-03-21 02:03:50 +0900

    nav 중간 commt

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+
+import './Sidebar.css';
+
+export default function Sidebar() {
+    return (
+        <nav>
+            <div className='todo-list-nav'>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+            <button className='nav-button'><img src='다음-icon.svg' /></button>
+        </nav>
+    )
+}
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,10 @@
+import Sidebar from "../components/Sidebar/Sidebar.jsx"
 import TodoItem from "../components/TodoItem/TodoItem.jsx"
 
 export default function Main() {
     return (
         <main>
+            <Sidebar />
             <ul>
                 <li className="todo-item-list"><TodoItem /></li>
                 <li className="todo-item-list"><TodoItem /></li>
