88fc09c34745f61cba7fcfd0d5ada7ef5b22b36a
Jaeyong <wodyd8672@gmail.com>
2024-03-22 19:13:06 +0900

    pagenation commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { modalActions } from '../../store';
 
@@ -6,6 +6,7 @@ import './Header.css';
 
 export default function Header() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     function openModal() {
         dispatch(modalActions.openModal());
@@ -17,8 +18,8 @@ export default function Header() {
             <div className="login">
                 <h2 onClick={openModal}>Login</h2>
                 <h2>Sign up</h2>
-                <button id='addTodo' onClick={openModal}>add Todo</button>
+                <button id='addTodo' onClick={() => navigate('edit')}>add Todo</button>
             </div>
-        </header>
+        </header >
     )
 } 
--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,11 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 
 import './TodoItem.css';
 
 export default function TodoItem(props) {
+    const navigate = useNavigate();
     const { id, category, title, startDate, endDate, todoList } = props
 
+
     return (
-        <div className="todo-item-container">
+        <div className="todo-item-container" onClick={() => navigate('detail')}>
             <div className="deadline">~ 마감 기한</div>
             <button className='todo-item-edit'>Edit</button>
             <h3>제목</h3>
